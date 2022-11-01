3bfe88b1da4ef7a6ec5e96baa6209f7b473d7203
Stéphane Klein <contact@stephane-klein.info>
2022-11-01 12:38:24 +0100

    Remove layout1

--- svelte-headlessui-tailwindcss-example1/src/routes/+page.svelte
+++ svelte-headlessui-tailwindcss-example1/src/routes/+page.svelte
@@ -1,5 +1,20 @@
-<ul>
-    <li>
-        <a href="layout1/">layout1</a>
-    </li>
-</ul>
+<div>
+    <article>
+        <header>
+            <div>
+                <h1>Event Types</h1>
+                <p>Create events to share for people to book on your calendar.</p>
+
+                <button>
+                    <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="inline-flex mr-2 h-4 w-4 stroke-[1.5px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                    New
+                </button>
+            </div>
+        </header>
+        <ul>
+            <li>15 min meeting</li>
+            <li>30 min meeting</li>
+            <li>Hidden meeting</li>
+        </ul>
+    </article>
+</div>
