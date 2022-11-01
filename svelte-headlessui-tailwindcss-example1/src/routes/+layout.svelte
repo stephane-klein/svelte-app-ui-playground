f03e36cc3fd4562dee2457392958e8d87ed2c688
Stéphane Klein <contact@stephane-klein.info>
2022-11-01 19:36:30 +0100

    WIP

--- svelte-headlessui-tailwindcss-example1/src/routes/+layout.svelte
+++ svelte-headlessui-tailwindcss-example1/src/routes/+layout.svelte
@@ -1,41 +1,23 @@
-<script>import "../app.postcss";</script><div>
-    <aside>
-        <div>
-            <header><h1>Example.com</h1></header>
-            <nav>
-                <a
-                    href="./"
-                    aria-label="Event Types"
-                    aria-current="page"
-                    class="selected"
-                >Event Types</a>
-                <a
-                    href="./bookings/"
-                    aria-label="Bookings"
-                >Bookings</a>
-                <a
-                    href="./availability/"
-                    aria-label="Availability"
-                >Availability</a>
-                <a
-                    href="./teams/"
-                    aria-label="Teams"
-                >Teams</a>
-                <a
-                    href="./apps/"
-                    aria-label="Apps"
-                >Apps</a>
-                <a
-                    aria-label="Workflows"
-                    href="./workflows/"
-                >Workflows</a>
-                <a
-                    aria-label="Settings"
-                    href="./settings/"
-                >Settings</a>
+<script>
+    import "../app.postcss";
+
+    import NavLink from './NavLink.svelte';
+</script>
+<div class="flex flex-row h-screen">
+    <aside class="flex flex-col">
+        <div class="flex-auto p-4">
+            <header class="px-1 py-2"><h1 class="m-0">Example.com</h1></header>
+            <nav class="flex flex-col">
+                <NavLink href="/">Event Types</NavLink>
+                <NavLink href="/bookings/">Bookings</NavLink>
+                <NavLink href="/availability/">Availability</NavLink>
+                <NavLink href="/teams/">Teams</NavLink>
+                <NavLink href="/apps/">Apps</NavLink>
+                <NavLink href="/workflows/">Workflows</NavLink>
+                <NavLink href="/settings/">Settings</NavLink>
             </nav>
         </div>
-        <button class="user-panel">
+        <button class="flex-none">
             <img
                 src="https://app.cal.com/stephaneklein/avatar.png"
                 alt="Avatar"
---
+++ svelte-headlessui-tailwindcss-example1/src/routes/NavLink.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	$: isActive = $page.url.pathname === $$props.href;
+    export let href;
+</script>
+
+<a
+    {href}
+    aria-label="Event Types"
+    class="my-1 px-2 py-1 hover:bg-gray-100 rounded"
+    class:bg-gray-300={isActive}
+><slot /></a>
