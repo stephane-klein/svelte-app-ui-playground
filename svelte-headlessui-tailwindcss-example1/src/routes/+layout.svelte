9008ad2a3b0b838f45eba17ce5ec852f279ed9f7
Stéphane Klein <contact@stephane-klein.info>
2022-11-02 16:51:03 +0100

    WIP

--- svelte-headlessui-tailwindcss-example1/src/routes/+layout.svelte
+++ svelte-headlessui-tailwindcss-example1/src/routes/+layout.svelte
@@ -4,7 +4,7 @@
     import NavLink from './NavLink.svelte';
 </script>
 <div class="flex flex-row h-screen">
-    <aside class="flex flex-col w-48">
+    <aside class="flex flex-col w-48 bg-gray-100 border border-r-gray-200">
         <div class="flex-auto px-6 py-4">
             <header class="px-1 py-2"><h1 class="m-0 text-xl font-bold">Example.com</h1></header>
             <nav class="flex flex-col mt-4">
@@ -17,13 +17,13 @@
                 <NavLink href="/settings/">Settings</NavLink>
             </nav>
         </div>
-        <button class="flex-none flex flex-row m-4 p-2 hover:bg-gray-100 rounded">
+        <button class="flex-none flex flex-row m-4 p-2 hover:bg-gray-100 rounded content-center">
             <img
                 src="https://app.cal.com/stephaneklein/avatar.png"
                 alt="Avatar"
                 class="h-8 mr-1"
             />
-            <span>John Doe</span>
+            <span class="leading-loose">John Doe</span>
         </button>
     </aside>
     <main class="p-6">
--- svelte-headlessui-tailwindcss-example1/src/routes/NavLink.svelte
+++ svelte-headlessui-tailwindcss-example1/src/routes/NavLink.svelte
@@ -7,6 +7,7 @@
 <a
     {href}
     aria-label="Event Types"
-    class="my-1 px-2 py-1 hover:bg-gray-100 rounded"
-    class:bg-gray-300={isActive}
+    class="my-1 px-3 py-2 text-sm font-medium hover:bg-gray-100 rounded-md text-gray-600"
+    class:bg-gray-200={isActive}
+    class:text-neutral-900={isActive}
 ><slot /></a>
